fdd295dc4f6f48a4c0e73d46f9717e4e412b0f2b
SerDieFer <serdiefer@alu.edu.gva.es>
2023-10-23 19:02:37 +0200

    28 iniciado

---
+++ PROGRAMMING/Tema 3/Ejercicios/Ejercicio 28/Ejercicio 28/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Ejercicio_28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.Location = new System.Drawing.Point(27, 31);
+            this.btnCal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(309, 58);
+            this.btnCal.TabIndex = 0;
+            this.btnCal.Text = "Calcular Suma";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 118);
+            this.Controls.Add(this.btnCal);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCal;
+    }
+}
+
---
+++ PROGRAMMING/Tema 3/Ejercicios/Ejercicio 28/Ejercicio 28/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            int mult,num1, num2;
+            string txt, txtSuma, txtNum1, txtNum2;
+
+            mult = 0;
+            txtNum1 = Interaction.InputBox("Introduzca el número a multiplicar");
+            txtNum2 = Interaction.InputBox("Introduzca el número a multiplicar");
+            txtSuma = "";
+            txt = "La multiplicación de " + txtNum1 + " x " + txtNum2 + " sería: \n\n";
+
+            num1 = int.Parse(txtNum1);
+            num2 = int.Parse(txtNum2);
+
+            if (num1 < 0 || num2 < 0)
+
+            {
+
+            num1 = -num1;
+            num2 = -num2;
+
+                for (int i = 0; i < num2; i++)
+
+                {
+
+                    if (i == num2 - 1)
+
+                    {
+                    txtSuma += num1.ToString();
+                    }
+
+                    else
+
+                    {
+                    txtSuma += num1 + " + ";
+                    }
+
+                }
+
+            }
+
+            else
+
+            {
+
+                for (int i = 0; i < num2; i++)
+
+                {
+                    if (i == num2 - 1)
+
+                    {
+                    txtSuma += num1.ToString(); ;   
+                    }
+
+                    else
+
+                    {
+                    txtSuma += num1 + " + ";
+                    }
+
+                }
+
+            }
+
+            mult = int.Parse(txtNum1) * int.Parse(txtNum2);
+            txt += num1 + " * " + num2 + " = " + txtSuma + " = " +  mult;
+
+            MessageBox.Show(txt);
+
+
+
+
+        }
+    }
+}
