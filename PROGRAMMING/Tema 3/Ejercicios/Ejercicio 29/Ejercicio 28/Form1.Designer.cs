eb7e3adf96fe0fbbffefa32e40a69d996f174d3a
SerDieFer <serdiefer@alu.edu.gva.es>
2023-10-24 20:37:49 +0200

    inicio 29

---
+++ PROGRAMMING/Tema 3/Ejercicios/Ejercicio 29/Ejercicio 28/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Ejercicio_28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCal = new System.Windows.Forms.Button();
+            this.lblEnunciado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.Location = new System.Drawing.Point(28, 117);
+            this.btnCal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(323, 30);
+            this.btnCal.TabIndex = 0;
+            this.btnCal.Text = "Calcular Serie";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // lblEnunciado
+            // 
+            this.lblEnunciado.AutoSize = true;
+            this.lblEnunciado.Location = new System.Drawing.Point(24, 37);
+            this.lblEnunciado.Name = "lblEnunciado";
+            this.lblEnunciado.Size = new System.Drawing.Size(341, 60);
+            this.lblEnunciado.TabIndex = 1;
+            this.lblEnunciado.Text = "Calcular el valor del siguiente serie de cálculos: \r\n\r\n1 - 1/2 + 1/3 - 1/4......." +
+    "(+ -) 1/n\r\n";
+            this.lblEnunciado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 176);
+            this.Controls.Add(this.lblEnunciado);
+            this.Controls.Add(this.btnCal);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 29";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Label lblEnunciado;
+    }
+}
+
---
+++ PROGRAMMING/Tema 3/Ejercicios/Ejercicio 29/Ejercicio 28/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            double num, suma, res, numerador;
+            string txtInput, txtSuma;
+
+            txtSuma = "La suma sería: \n\n";
+            txtInput = Interaction.InputBox("Esto significará que se realizará la misma serie de cálculos hasta donde " +
+            "indique el usuario mediante el número a elegir");
+
+            res = 0;
+            num = double.Parse(txtInput);
+ 
+
+            if (num > 1)
+
+            {
+                suma = 0;
+
+                   for (double i = 1; i <= num; i++)
+
+                   {
+
+                    numerador = 1.0 / i;
+
+                        if (i % 2 == 0)
+
+                        {
+                            suma -= numerador;
+                            txtSuma += " - ";
+                        }
+
+                        else
+
+                        {
+                            suma += numerador;
+                            txtSuma += " + ";
+                        }
+
+
+                    txtSuma += suma.ToString();
+
+
+                }
+
+                 res = suma;
+                 MessageBox.Show("1 " + txtSuma + " = " + res);
+            }
+        }
+    }
+}
+
+
