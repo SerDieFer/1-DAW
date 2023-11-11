98300db75389a21d33fd013c96d292cc12591233
SerDieFer <145585172+SerDieFer@users.noreply.github.com>
2023-11-11 23:17:04 +0100

    14a y 14b

---
+++ PROGRAMMING/Tema 4/Ejercicio 14a/Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultados = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(91, 76);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(54, 20);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Valor :";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(151, 73);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(143, 26);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnCalcular.Location = new System.Drawing.Point(95, 129);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(199, 29);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultados
+            // 
+            this.lblResultados.AutoSize = true;
+            this.lblResultados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultados.Location = new System.Drawing.Point(126, 201);
+            this.lblResultados.Name = "lblResultados";
+            this.lblResultados.Size = new System.Drawing.Size(0, 20);
+            this.lblResultados.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 478);
+            this.Controls.Add(this.lblResultados);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblValor);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 14a";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultados;
+    }
+}
+
---
+++ PROGRAMMING/Tema 4/Ejercicio 14a/Ejercicio 1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            string CalcularPesetas(ref int valorIntroducido, ref int cantidadBilletes)
+            {
+
+                string txt = "Le corresponden:\n\n";
+
+
+                while (valorIntroducido > 25)
+                {
+                    cantidadBilletes = valorIntroducido / 10000;
+
+                    if (cantidadBilletes > 0)
+                    {
+                        txt += cantidadBilletes + " billetes de 10000\n";
+                    }
+
+                    valorIntroducido = valorIntroducido % 10000;
+
+                    if (valorIntroducido < 10000)
+                    {
+                        cantidadBilletes = valorIntroducido / 5000;
+
+                        if (cantidadBilletes > 0)
+                        {
+                            txt += cantidadBilletes + " billetes de 5000\n";
+                        }
+                   
+                        valorIntroducido = valorIntroducido % 5000;
+
+                        if (valorIntroducido < 5000)
+                        {
+                            cantidadBilletes = valorIntroducido / 2000;
+
+                            if (cantidadBilletes > 0)
+                            {
+                                txt += cantidadBilletes + " billetes de 2000\n";
+                            }
+
+                            valorIntroducido = valorIntroducido % 2000;
+
+                            if (valorIntroducido < 2000)
+                            {
+                                cantidadBilletes = valorIntroducido / 1000;
+
+                                if (cantidadBilletes > 0)
+                                {
+                                    txt += cantidadBilletes + " billetes de 1000\n";
+                                }
+
+                                valorIntroducido = valorIntroducido % 1000;
+
+                                if (valorIntroducido < 1000)
+                                {
+                                    cantidadBilletes = valorIntroducido / 100;
+
+                                    if (cantidadBilletes > 0)
+                                    {
+                                        txt += cantidadBilletes + " moneda de 100\n";
+                                    }
+
+                                    valorIntroducido = valorIntroducido % 100;
+
+                                    if (valorIntroducido < 100)
+                                    {
+                                        cantidadBilletes = valorIntroducido / 25;
+
+                                        if (cantidadBilletes > 0)
+                                        {
+                                            txt += cantidadBilletes + " monedas de 25\n";
+                                        }
+
+                                        valorIntroducido = valorIntroducido % 25;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return txt;
+            }
+
+            private void btnCalcular_Click(object sender, EventArgs e)
+            {
+                //Aqui declaramos las variables
+                int valorIntroducido, cantidadBilletes;
+
+                valorIntroducido = int.Parse(txtValor.Text);
+                cantidadBilletes = valorIntroducido;
+
+                lblResultados.Text = CalcularPesetas(ref valorIntroducido, ref cantidadBilletes);
+
+            }
+        }
+    }
+
+
