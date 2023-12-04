a8591b5d7139714804e1c3d222d767c711ff93df
SerDieFer <serdiefer@alu.edu.gva.es>
2023-12-04 18:59:01 +0100

    arrays

---
+++ PROGRAMMING/Tema 5/Ejercicio 3/Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(12, 12);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(218, 41);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(12, 75);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(218, 39);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar Menor";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 126);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ PROGRAMMING/Tema 5/Ejercicio 4/Ejercicio 1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] vector = new int[10];
+
+        void LeerVector(int[] vector)
+        {
+            for(int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introduce el valor [" + i + "]:"));
+            }
+        }
+
+        int CambioNegativo(int[] vector)
+        {
+
+            int numNegativos = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] < 0)
+                {
+                    numNegativos++;
+                }
+                else
+                {
+                    vector[i] = 0;
+                }
+            }
+            return numNegativos;
+        }
+
+        string MostrarVector(int[] vector)
+        {
+            string txt = "Los valores del vector son: ";
+            
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (i == 9)
+                {
+                    txt += vector[i] + ". ";
+                }
+                else
+                {
+                    txt += vector[i] + ", ";
+                }
+            }
+            return txt;
+        }
+
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            LeerVector(vector);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string txtVector = MostrarVector(CambioNegativo(vector)).ToString();
+            MessageBox.Show(txtVector);
+        }
+    }
+}
