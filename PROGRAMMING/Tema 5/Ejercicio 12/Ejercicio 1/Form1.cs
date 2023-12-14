fd838f16fcc92ef8ecd07d773c4ecb1e6aabf5cc
SerDieFer <serdiefer@alu.edu.gva.es>
2023-12-14 19:00:06 +0100

    Programacion 12-13

---
+++ PROGRAMMING/Tema 5/Ejercicio 12/Ejercicio 1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[] horas = new double[24];
+        void LeerVector(double[] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = double.Parse(Interaction.InputBox("Introduce la temperatura a las [" + i + "]:"));
+            }
+        }  
+   
+        void TemperaturasMedia(double[] vector, out double tMax, out double tMin, out double tMedia)
+        {
+            tMedia = tMin = tMax = vector[0];
+        
+            double sumaTemp = 0;
+
+
+            for (int i = 1; i < vector.Length; i++)
+            {
+
+                if (vector[i] > tMax)
+                {
+                    tMax = vector[i];
+                }
+
+                else if (vector[i] < tMin)
+                {
+                    tMin = vector[i];
+                }
+
+                sumaTemp += vector[i];
+            }
+
+            tMedia = sumaTemp / vector.Length;
+            tMedia = Math.Round(tMedia, 2);
+
+        }
+
+
+        string MostrarVector(double[] vector)
+        {
+            string txt = "Los valores del vector son: ";
+
+            for(int i = 0; i < vector.Length; i++)
+            {
+            
+                if (i == (vector.Length - 1))
+                {
+                    txt += vector[i] + ".";
+                }
+                else
+                {
+                    txt += vector[i] + ", ";
+                }
+            }
+
+            return txt;
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            LeerVector(horas);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            double tMin, tMax, tMedia;
+
+            TemperaturasMedia(horas, out tMax, out tMin, out tMedia);
+
+            string txtRes, txtResTemp;
+
+            txtRes = MostrarVector(horas);
+            txtResTemp = "\n\nLa temperatura mínima será: " + tMin + "\nLa temperatura máxima será: " + tMax +
+                "\nLa temperatura media será: " + tMedia;
+                 
+            MessageBox.Show(txtRes + txtResTemp);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PROGRAMMING/Tema 5/Ejercicio 13/Ejercicio 1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] numeros = new int[2];
+        void LeerVector(int[] vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = int.Parse(Interaction.InputBox("Introduce el valor de la posición [" + i + "] en el vector:"));
+            }
+        }
+
+        bool Comprobar(int[] vector)
+        {
+
+            bool estaDentro = false;
+            int numComparar = int.Parse(Interaction.InputBox("Introduce el número a comprobar en el vector"));
+           
+            for(int i = 0; i < vector.Length && !estaDentro;i++)
+            {
+                if(numComparar == vector[i])
+                {
+                    estaDentro = true;
+                }
+            }
+
+            return estaDentro;
+        }
+
+        string MostrarVector(int[] vector)
+        {
+            string txt = "Los valores del vector son: ";
+
+            for(int i = 0; i < vector.Length; i++)
+            {
+            
+                if (i == (vector.Length - 1))
+                {
+                    txt += vector[i] + ".";
+                }
+                else
+                {
+                    txt += vector[i] + ", ";
+                }
+            }
+
+            return txt;
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            LeerVector(numeros);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+
+            string txtRes;
+            txtRes = MostrarVector(numeros);
+            MessageBox.Show(txtRes);
+
+            if (Comprobar(numeros))
+            {
+                MessageBox.Show("El número está dentro del vector");
+            }
+
+            else
+            {
+                MessageBox.Show("El número NO está dentro del vector");
+            }
+
+
+            
+
+        }
+    }
+}
