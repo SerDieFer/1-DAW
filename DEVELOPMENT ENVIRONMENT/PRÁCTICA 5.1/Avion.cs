4344393ab97d98299e58c6a0b38ae51712ea6646
SerDieFer <145585172+SerDieFer@users.noreply.github.com>
2024-01-28 20:43:32 +0100

    entolnos

---
+++ DEVELOPMENT ENVIRONMENT/PRÁCTICA 5.1/Avion.cs
@@ -0,0 +1,73 @@
+public class Avion
+{
+    private float altura;
+    private float velocidad;
+    private float combustible;
+    private int orientación;
+
+    public Avion(float altura, float velocidad, float combustible, int orientacion)
+    {
+        this.altura = altura; 
+        this.velocidad = velocidad;
+        this.combustible = combustible; 
+        this.orientación = orientacion;
+    }
+
+    public float Altura
+    {
+        get 
+        { 
+            return altura; 
+        }
+    }
+
+    public int Orientacion
+    {
+        get
+        {
+            return orientación;
+        }
+    }
+
+    public float Combustible
+    { 
+        get 
+        { 
+            return combustible; 
+        } 
+    }
+
+    //Metodo que sirve para girar respecto a los grados indicados.
+    private void Virar(int grados)
+    {
+        orientación = (orientación + grados) % 360;
+        ConsumirFuel(grados * 0.1);
+    }
+
+    //Metodo que sirve para ascender respecto a los metros indicados.
+    private void Ascender(float metros)
+    {
+        altura += metros;
+        ConsumirFuel(metros * 0.3);
+    }
+
+    //Metodo que sirve para descender respecto los metros indicados.
+    private void Descender(float metros)
+    {
+        altura -= metros;
+        if (altura < 0)
+        {
+            altura = 0;
+        }
+    }
+
+    //Metodo que sirve para consumir combustible respecto los litros indicados.
+    private void ConsumirFuel(float litros)
+    {
+        combustible -= litros;
+        if (combustible < 0)
+        {
+            combustible = 0;
+        }
+    }
+}
