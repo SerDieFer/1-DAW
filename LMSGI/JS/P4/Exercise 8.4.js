13f92bc2ca28cf64e61079e5a734c75329eeaa65
SerDieFer <serdiefer@alu.edu.gva.es>
2024-02-21 20:48:36 +0100

    PROGRAMACION

---
+++ LMSGI/JS/P4/Exercise 8.4.js
@@ -0,0 +1,35 @@
+const tableContainer = document.getElementById("table");
+const createdTable = createTable(getRowCount(), getColumnCount());
+tableContainer.appendChild(createdTable);
+    
+function createTable(rowCount,columnCount) {
+    const table = [] 
+    for (let x = 0; x < columnCount; x++) 
+    {
+        table[x] = []
+        for (let y = 0; y < rowCount; y++) 
+        {
+            addCell(table, x, y);
+        }
+    }       
+return table
+}
+
+function addCell(table, x, y) {
+    table[x][y] = cell();
+}
+
+function cell() {
+    return document.createElement("td");
+}
+
+function getRowCount() {
+               let numberOfRows = parseInt(prompt("Elige el número de filas"))
+               return numberOfRows
+               }
+
+function getColumnCount() {
+                let numberOfColumns = parseInt(prompt("Elige el número de columnas"))
+                return numberOfColumns
+                }
+
