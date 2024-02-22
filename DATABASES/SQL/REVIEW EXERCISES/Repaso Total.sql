6110cd588c419df3833774de56583bb390510e87
SerDieFer <145585172+SerDieFer@users.noreply.github.com>
2024-02-22 14:38:55 +0100

    bd

--- DATABASES/SQL/REVIEW EXERCISES/Repaso Total.sql
+++ DATABASES/SQL/REVIEW EXERCISES/Repaso Total.sql
@@ -1,6 +1,9 @@
 /*
-CREACIÓN / MODIFICACIÓN DE TABLAS
+---------------------------------------
+-- CREACIÓN | MODIFICACIÓN DE TABLAS --
+---------------------------------------
 Crea las siguientes tablas:
+
 STREAMERS (codStreamer, nombre, apellidos, pais, edad)
 PK: codStreamer (NO se incrementa automáticamente)
 
@@ -13,105 +16,375 @@ FK: codStreamer  STREAMERS
 FK: codTematica  TEMATICAS
 */
 
+CREATE DATABASE REPASO
+USE REPASO
+GO
+CREATE TABLE STREAMERS (
+    codStreamer INT,
+    nombre VARCHAR(100) NOT NULL,
+    apellidos VARCHAR(100),
+    pais CHAR(2),
+    edad TINYINT NOT NULL
 
+    CONSTRAINT PK_STREAMERS PRIMARY KEY (codStreamer)
+    CONSTRAINT CH_STREAMERS_edad CHECK (edad BETWEEN 1 AND 100)
+)
+GO
+CREATE INDEX streamers_name_index
+ON STREAMERS (nombre)
+GO
+CREATE TABLE TEMATICAS (
+    codTematica INT IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL
 
+    CONSTRAINT PK_TEMATICAS PRIMARY KEY (codTematica)
+)
+GO
+CREATE INDEX tematica_name_index
+ON TEMATICAS (nombre)
+GO
+CREATE TABLE STREAMERS_TEMATICAS (
+    codStreamer INT,
+    codTematica INT,
+    idioma VARCHAR(100) NOT NULL,
+    medio VARCHAR(100) NOT NULL,
+    milesSeguidores INT NOT NULL
 
-
-
-
-
+    CONSTRAINT PK_STREAMERS_TEMATICAS PRIMARY KEY (codStreamer,codTematica),
+    CONSTRAINT FK_STREAMERS_TEMATICAS_STREAMERS FOREIGN KEY (codStreamer) 
+               REFERENCES STREAMERS(codStreamer),
+    CONSTRAINT FK_STREAMERS_TEMATICAS_TEMATICAS FOREIGN KEY (codTematica)
+               REFERENCES TEMATICAS(codTematica)
+)
+GO
+CREATE INDEX streamers_tematicas_medio_index
+ON STREAMERS_TEMATICAS (codStreamer, medio, milesSeguidores)
+GO
 
 /*
-GESTIÓN DE TABLAS
-Inserta los siguientes STREAMERS:
--	Ibai Llanos de España
--	AuronPlay de España
--	Nate Gentile de España
--	Linus Tech Tips de Canadá
--	DYI Perks sin ningún país
--	Alexandre Chappel de Noruega
--	Tekendo de España
--	Caddac Tech de ningún país
-
-Inserta los siguientes TEMAS:
--	Informática
--	Tecnología en general
--	Gaming
--	Variado
--	Bricolaje
--	Viajes
--	Humor
-
-
-Inserta las siguientes TEMATICAS de STREAMERS:
-STREAMER	TEMATICA	idioma	medio	milesSeguidores
-AuronPlay	Gaming	Español	YouTube	29200
-Ibai Llanos	Variado	Español	Twitch	12800
-AuronPlay	Variado	Español	Twitch	14900
-Nate Gentile	Informática	Español	YouTube	2450
-Linus Tech Tips	Informática	Inglés	YouTube	15200
-DYI Perks	Bricolaje	Inglés	YouTube	4140
-Alexandre Chappel	Bricolaje	Inglés	YouTube	370
-Caddac Tech	Informática	Inglés	YouTube	3
+-----------------------
+-- GESTIÓN DE TABLAS --
+-----------------------
+Inserta los siguientes STREAMERS:   Inserta los siguientes TEMAS:    Inserta las siguientes TEMATICAS de STREAMERS:
+    - Ibai Llanos de España             - Informática                   [STREAMER]           [TEMATICA]     [idioma]   [medio]	  [milesSeguidores]
+    - AuronPlay de España               - Tecnología en general         Ibai Llanos	         Variado	    Español	   Twitch     12800
+    - Nate Gentile de España            - Gaming                        AuronPlay	         Gaming	        Español    YouTube    29200
+    - Linus Tech Tips de Canadá         - Variado                       AuronPlay	         Variado	    Español	   Twitch     14900
+    - DYI Perks sin ningún país         - Bricolaje                     Nate Gentile         Informática    Español    YouTube	  2450
+    - Alexandre Chappel de Noruega      - Viajes                        Linus Tech Tips      Informática	Inglés	   YouTube	  15200
+    - Tekendo de España                 - Humor                         DYI Perks	         Bricolaje	    Inglés	   YouTube	  4140
+    - Caddac Tech de ningún país                                        Alexandre Chappel    Bricolaje	    Inglés	   YouTube	  370
+                                                                        Caddac Tech	         Informática	Inglés	   YouTube    3
 */
 
+GO
+INSERT INTO STREAMERS (codStreamer, nombre, apellidos, pais, edad)
+VALUES (1, 'Ibai', 'Llanos', 'ES', 28),
+       (2, 'AuronPlay', NULL, 'ES', 35),
+       (3, 'Nate', 'Gentile', 'ES', 33),
+       (4, 'Linus','Tech Tips', 'CA', 37),
+       (5, 'DYI Perks', NULL, NULL, 37),
+       (6, 'Alexandre', 'Chappel', 'NO', 37),
+       (7, 'Tekendo', NULL, 'ES', 26),
+       (8, 'Caddac Tech', NULL, NULL, 40),
+       (9, 'Abuelo', NULL, NULL, 40)
+GO
+INSERT INTO TEMATICAS (nombre)
+VALUES ('Informática'),
+       ('Tecnología en general'),
+       ('Gaming'),
+       ('Variado'),
+       ('Bricolaje'),
+       ('Viajes'),
+       ('Humor')
+GO
+INSERT INTO STREAMERS_TEMATICAS (codStreamer, codTematica, idioma, medio, milesSeguidores)
+VALUES (1, 4, 'Español', 'Twitch', 12800),
+       (2, 3, 'Español', 'YouTube', 29200),
+       (2, 4, 'Español', 'Twitch', 14900),
+       (3, 1, 'Español', 'YouTube', 2450),
+       (4, 1, 'Inglés', 'YouTube', 15200),
+       (5, 5, 'Inglés', 'YouTube', 4140),
+       (6, 5, 'Inglés', 'YouTube', 370),
+       (8, 1, 'Inglés', 'YouTube', 3)
 
 -----------------
 --  CONSULTAS  --
 -----------------
+
 -- 01. Nombre de las temáticas que tenemos almacenadas, ordenadas alfabéticamente.
+
+SELECT nombre
+  FROM TEMATICAS
+ ORDER BY nombre ASC
 
 -- 02. Cantidad de streamers cuyo país es "España".
 
+SELECT COUNT(codStreamer)
+  FROM STREAMERS
+ WHERE LOWER(pais) = 'es'
+
 -- 03, 04, 05. Nombres de streamers cuya segunda letra no sea una "B" (quizá en minúsculas), de 3 formas distintas.
+
+SELECT nombre
+  FROM STREAMERS
+ WHERE LOWER(SUBSTRING(nombre,2,1)) <> ('b')
+
+SELECT nombre
+  FROM STREAMERS
+ WHERE RIGHT(LEFT(nombre, 2),1) NOT IN ('b', 'B')
+
+SELECT nombre
+  FROM STREAMERS
+ WHERE LOWER(nombre) NOT LIKE ('%b%')
+
 
 -- 06. Media de suscriptores para los canales cuyo idioma es "Español".
 
+SELECT AVG(milesSeguidores) AS mediaSeguidoresCanalesEspañoles
+  FROM STREAMERS_TEMATICAS
+ WHERE LOWER(idioma) = 'español'
+
 -- 07. Media de seguidores para los canales cuyo streamer es del país "España".
+
+SELECT AVG(milesSeguidores) AS mediaSeguidoresCanalesDeStreamerEspañol
+  FROM STREAMERS_TEMATICAS 
+ WHERE codStreamer IN (SELECT codStreamer
+                         FROM STREAMERS
+                         WHERE UPPER(pais) = 'ES')
+
+ SELECT AVG(milesSeguidores) AS mediaSeguidoresCanalesDeStreamerEspañol
+   FROM STREAMERS s,
+        STREAMERS_TEMATICAS st
+  WHERE s.codStreamer = st.codStreamer
+    AND UPPER(pais) = 'ES'
 
 -- 08. Nombre de cada streamer y medio en el que habla, para aquellos que tienen entre 5.000 y 15.000 miles de seguidores, usando BETWEEN.
 
+SELECT s.nombre,
+       st.medio
+  FROM STREAMERS s,
+       STREAMERS_TEMATICAS st
+ WHERE s.codStreamer = st.codStreamer
+   AND milesSeguidores BETWEEN 5000 AND 15000
+
 -- 09. Nombre de cada streamer y medio en el que habla, para aquellos que tienen entre 5.000 y 15.000 miles de seguidores, sin usar BETWEEN.
 
+SELECT s.nombre,
+       st.medio
+  FROM STREAMERS s,
+       STREAMERS_TEMATICAS st
+ WHERE s.codStreamer = st.codStreamer
+   AND milesSeguidores > 5000   
+   AND milesSeguidores < 15000
+
 -- 10. Nombre de cada temática y nombre de los idiomas en que tenemos canales de esa temática 
---(quizá ninguno), sin duplicados.
+    --(quizá ninguno), sin duplicados.
+
+SELECT DISTINCT t.nombre AS nombreTematica,
+       st.idioma AS idiomaCanal
+  FROM STREAMERS_TEMATICAS st
+  LEFT JOIN TEMATICAS t
+    ON st.codTematica = t.codTematica
+ ORDER BY st.idioma ASC
 
 -- 11. Nombre de cada streamer, nombre de la temática de la que habla y del medio en el que habla de esa temática, usando INNER JOIN.
 
+SELECT s.nombre AS nombreStreamer,
+       st.medio AS medioStreamer,
+       t.nombre AS nombreTematicaCanal
+  FROM TEMATICAS t
+ INNER JOIN STREAMERS_TEMATICAS st
+    ON t.codTematica = st.codTematica
+ INNER JOIN  STREAMERS s
+    ON st.codStreamer = s.codStreamer
+
 -- 12. Nombre de cada streamer, nombre de la temática de la que habla y del medio en el que habla de esa temática, usando WHERE.
 
--- 13. Nombre de cada streamer, del medio en el que habla y de la temática de la que habla en ese medio, incluso si de algún streamer no tenemos dato del medio o de la temática.
+SELECT s.nombre AS nombreStreamer,
+       st.medio AS medioStreamer,
+       t.nombre AS nombreTematicaCanal
+FROM STREAMERS s,
+     TEMATICAS t,
+     STREAMERS_TEMATICAS st
+WHERE s.codStreamer = st.codStreamer
+  AND t.codTematica = st.codTematica
+
+-- 13. Nombre de cada streamer, del medio en el que habla y de la temática de la que habla en ese medio, 
+-- incluso si de algún streamer no tenemos dato del medio o de la temática.
+
+
+SELECT s.nombre AS nombreStreamer,
+       st.medio AS medioStreamer,
+       t.nombre AS nombreTematicaCanal
+  FROM STREAMERS s
+  LEFT JOIN STREAMERS_TEMATICAS st
+    ON s.codStreamer = st.codStreamer
+ LEFT JOIN TEMATICAS t
+    ON st.codTematica = t.codTematica
 
 -- 14. Nombre de cada medio y cantidad de canales que tenemos anotados en él, ordenado alfabéticamente por el nombre del medio.
 
+SELECT medio,
+       COUNT(codStreamer) AS numCanales
+  FROM STREAMERS_TEMATICAS
+ GROUP BY medio
+ ORDER BY medio ASC
+
 -- 15, 16, 17, 18. Medio en el que se emite el canal de más seguidores, de 4 formas distintas.
+
+SELECT medio
+  FROM STREAMERS_TEMATICAS
+ WHERE milesSeguidores = (SELECT MAX(milesSeguidores)
+                            FROM STREAMERS_TEMATICAS)
+SELECT TOP(1) medio
+  FROM STREAMERS_TEMATICAS
+ ORDER BY milesSeguidores DESC
+
+SELECT medio
+  FROM STREAMERS_TEMATICAS
+ WHERE milesSeguidores >= ALL (SELECT milesSeguidores
+                                FROM STREAMERS_TEMATICAS)
 
 -- 19. Categorías de las que tenemos 2 o más canales.
 
+SELECT nombre
+  FROM TEMATICAS
+ WHERE codTematica IN (SELECT codTematica
+                         FROM STREAMERS_TEMATICAS
+                     GROUP BY codTematica
+                       HAVING COUNT(codStreamer) >= 2)
+
 -- 20. Categorías de las que no tenemos anotado ningún canal, ordenadas alfabéticamente, empleando COUNT.
+
+SELECT t.nombre
+  FROM TEMATICAS t
+  LEFT JOIN STREAMERS_TEMATICAS st 
+    ON t.codTematica = st.codTematica
+ GROUP BY t.codTematica, t.nombre
+HAVING COUNT(st.codTematica) = 0
+ ORDER BY t.nombre ASC;
 
 -- 21. Categorías de las que no tenemos anotado ningún canal, ordenadas alfabéticamente, empleando IN / NOT IN.
 
+SELECT nombre
+  FROM TEMATICAS
+ WHERE codTematica NOT IN (SELECT codTematica
+                             FROM STREAMERS_TEMATICAS)
+ORDER BY nombre ASC
+
 -- 22. Categorías de las que no tenemos anotado ningún canal, ordenadas alfabéticamente, empleando ALL / ANY.
+
+SELECT nombre
+  FROM TEMATICAS
+ WHERE codTematica <> ALL (SELECT codTematica
+                             FROM STREAMERS_TEMATICAS)
+ORDER BY nombre ASC
 
 -- 23. Categorías de las que no tenemos anotado ningún canal, ordenadas alfabéticamente, empleando EXISTS / NOT EXISTS.
 
+SELECT t.nombre
+  FROM TEMATICAS t
+ WHERE NOT EXISTS (SELECT st.codStreamer
+                     FROM STREAMERS_TEMATICAS st
+                    WHERE st.codTematica = t.codTematica)
+ ORDER BY t.nombre ASC
+
 -- 24. Tres primeras letras de cada país y tres primeras letras de cada idioma, en una misma lista.
+
+SELECT LEFT(s.pais, 1) AS UnaLetrasPais
+  FROM STREAMERS s
+ WHERE s.pais IS NOT NULL
+ UNION
+SELECT LEFT(st.idioma, 3) AS TresLetrasIdioma
+  FROM STREAMERS_TEMATICAS st
+ WHERE st.idioma IS NOT NULL
 
 -- 25, 26, 27, 28. Tres primeras letras de países que coincidan con las tres primeras letras de un idioma, sin duplicados, de cuatro formas distintas.
 
+SELECT DISTINCT LEFT(pais, 3) AS letrasPais
+  FROM STREAMERS s
+ INNER JOIN STREAMERS_TEMATICAS st
+    ON s.codStreamer = st.codStreamer
+ WHERE LEFT(s.pais, 3) = LEFT(st.idioma, 3)
+
+SELECT DISTINCT LEFT(pais, 3) AS letrasPais
+  FROM STREAMERS
+ WHERE LEFT(pais, 3) IN (SELECT LEFT(idioma, 3)
+                           FROM STREAMERS_TEMATICAS)
+
+SELECT DISTINCT LEFT(pais, 3) AS letrasPais
+  FROM STREAMERS
+ WHERE LEFT(pais, 3) = ANY (SELECT LEFT(idioma, 3)
+                             FROM STREAMERS_TEMATICAS)
+
+SELECT DISTINCT LEFT(s.pais, 3) AS letrasPais
+FROM STREAMERS s
+WHERE EXISTS (SELECT *
+                FROM STREAMERS_TEMATICAS st
+               WHERE st.codStreamer = s.codStreamer
+                 AND LEFT(st.idioma, 3) = LEFT(s.pais, 3))
+
 -- 29. Nombre de streamer, nombre de medio y nombre de temática, para los canales que están por encima de la media de suscriptores.
+
+SELECT s.nombre,
+       st.medio,
+       t.nombre
+  FROM STREAMERS s,
+       STREAMERS_TEMATICAS st,
+       TEMATICAS t
+ WHERE s.codStreamer = st.codStreamer
+   AND t.codTematica = st.codTematica
+   AND st.milesSeguidores > (SELECT AVG(milesSeguidores) 
+                               FROM STREAMERS_TEMATICAS)
 
 -- 30. Nombre de streamer y medio, para los canales que hablan de la temática "Bricolaje".
 
+SELECT s.nombre,
+       st.medio
+  FROM STREAMERS s,
+       STREAMERS_TEMATICAS st,
+       TEMATICAS t
+ WHERE s.codStreamer = st.codStreamer
+   AND t.codTematica = st.codTematica
+   AND LOWER(t.nombre) = 'bricolaje'
+
 -- 31. Crea una tabla de "juegos". Para cada juego querremos un código (clave primaria), un nombre (hasta 20 letras, no nulo) y una referencia al streamer que más habla de él (clave ajena a la tabla "streamers").
+
+CREATE TABLE JUEGOS (
+    codJuego INT,
+    nombre VARCHAR(20) NOT NULL,
+    codStreamer INT
+
+    CONSTRAINT PK_JUEGOS PRIMARY KEY (codJuego)
+    CONSTRAINT FK_JUEGOS_STREAMERS FOREIGN KEY (codStreamer)
+               REFERENCES STREAMERS(codStreamer)
+)   
 
 -- 32. Añade a la tabla de juegos la restricción de que el código debe ser 1000 o superior.
 
+ALTER TABLE JUEGOS 
+ADD CONSTRAINT CK_JUEGOS_codJuego CHECK (codJuego >= 1000)
+
+
 -- 33. Añade 3 datos de ejemplo en la tabla de juegos. Para uno indicarás todos los campos, para otro no indicarás el streamer, ayudándote de NULL, y para el tercero no indicarás el streamer porque no detallarás todos los nombres de los campos.
+
+INSERT INTO JUEGOS (codJuego, nombre, codStreamer)
+VALUES (1001, 'NombreJuego1', 1);
+
+INSERT INTO JUEGOS (codJuego, nombre, codStreamer)
+VALUES (1002, 'NombreJuego2', NULL);
+
+INSERT INTO JUEGOS
+VALUES (1003, 'NombreJuego3', NULL);
+
+SELECT *
+FROM JUEGOS
 
 -- 34. Borra el segundo dato de ejemplo que has añadido en la tabla de juegos, a partir de su código.
 
+DELETE FROM JUEGOS
+WHERE codJuego = 1002
 
 -- 35. Muestra el nombre de cada juego junto al nombre del streamer que más habla de él, si existe. Los datos aparecerán ordenados por nombre de juego y, en caso de coincidir éste, por nombre de streamer.
 
