232e66b3084ae160c569ea492e6173f2547f8a77
SerDieFer <serdiefer@alu.edu.gva.es>
2024-03-21 20:46:53 +0100

    PG

--- PROGRAMMING/Tema 8/Exercise 5/Exercise 5/PersonList.cs
+++ PROGRAMMING/Tema 8/Exercise 5/Exercise 5/PersonList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,185 @@ namespace Exercise_5
         public PersonList()
         {
             personList = new List<Person>();
+        }
+
+        // ALUMNS RELATED METHODS //
+
+
+        public string ShowsAlumnsWhoseGradesAvgIsEqualOrBiggerThan5()
+        {
+            string gradesMoreThan5 = "The alumns with the grade equal or bigger than 5 are: \n\n";
+            if (alumnList.Count != 0)
+            {
+                for (int i = 0; i < alumnList.Count; i++)
+                {
+                    if (alumnList[i].AlumnGradesAverage() >= 5 && SelectedAlumnHasGrades(alumnList[i].ID))
+                    {
+                        gradesMoreThan5 += alumnList[i].ShowsAlumnData() + "\n";
+                    }
+                }
+            }
+            return gradesMoreThan5;
+        }
+
+        // FUNCTION WHICH COUNTS HOW MANY ALUMNS HAS AN AVERAGE EQUAL OR MORE THAN A 5
+        public int CountAlumnsWhoseGradeIsEqualOrBiggerThan5()
+        {
+            int alumnGradesMoreThan5 = 0;
+            if (alumnList.Count != 0)
+            {
+                for (int i = 0; i < alumnList.Count; i++)
+                {
+                    if (alumnList[i].AlumnGradesAverage() >= 5 && SelectedAlumnHasGrades(alumnList[i].ID))
+                    {
+                        alumnGradesMoreThan5++;
+                    }
+                }
+            }
+            return alumnGradesMoreThan5;
+        }
+
+        // FUNCTION WHICH RETURNS ALL ALUMNS WHOSE AVERAGE GRADE IS LOWER THAN 5
+        public string ShowsAlumnsWhoseGradesAvgIsLowerThan5()
+        {
+            string gradesLowerThan5 = "The alumns with the grade lower than 5 are: \n\n";
+            if (alumnList.Count != 0)
+            {
+                for (int i = 0; i < alumnList.Count; i++)
+                {
+                    if (alumnList[i].AlumnGradesAverage() < 5 && SelectedAlumnHasGrades(alumnList[i].ID))
+                    {
+                        gradesLowerThan5 += alumnList[i].ShowsAlumnData() + "\n";
+                    }
+                }
+            }
+            return gradesLowerThan5;
+        }
+
+        // FUNCTION WHICH COUNTS HOW MANY ALUMNS HAS AN AVERAGE LOWER THAN A 5
+        public int CountAlumnsWhoseGradeIsLowerThan5()
+        {
+            int alumnGradesLowerThan5 = 0;
+            if (alumnList.Count != 0)
+            {
+                for (int i = 0; i < alumnList.Count; i++)
+                {
+                    if (alumnList[i].AlumnGradesAverage() < 5 && SelectedAlumnHasGrades(alumnList[i].ID))
+                    {
+                        alumnGradesLowerThan5++;
+                    }
+                }
+            }
+            return alumnGradesLowerThan5;
+        }
+
+        // FUNCTION WHICH RETURNS ALL ALUMNS WHO ARE IN THE SELECTED COURSE
+        public string ShowAlumnsBySelectedCourseCod(int courseCod)
+        {
+            string alumnsInCourse = "The alumns in the course (" + courseCod + ") are: \n\n";
+            if (personList.Count != 0)
+            {
+                for (int i = 0; i < personList.Count; i++)
+                {
+                    if (((Alumn)personList[i]).CourseCod == courseCod)
+                    {
+                        alumnsInCourse += personList[i].ShowsPersonData() + "\n";
+                    }
+                }
+            }
+            return alumnsInCourse;
+        }
+
+        // FUNCTION THAT ORDERS ALUMNS IN ALPHABETICAL ORDER
+        public void SortAlumnListAlphabetically()
+        {
+            Alumn auxAlumnList;
+            for (int i = 0; i < alumnList.Count; i++)
+            {
+                for (int j = i + 1; j < alumnList.Count; j++)
+                {
+                    if (string.Compare(alumnList[i].Name, alumnList[j].Name) > 0)
+                    {
+                        auxAlumnList = alumnList[i];
+                        alumnList[i] = alumnList[j];
+                        alumnList[j] = auxAlumnList;
+                    }
+                }
+            }
+        }
+
+
+        public bool AlumnInCourse(int aCourseCod)
+        {
+            bool alumnInCourse = false;
+
+            for (int i = 0; i < personList.Count && !alumnInCourse; i++)
+            {
+                string personID = ReturnsPersonIDFromPosition(i);
+                int personType = CheckTypeOfPerson(personID);
+
+                // TYPE 0 IS ALUMN
+                if (personType == 0)
+                {
+                    if (aCourseCod == ((Alumn)personList[i]).CourseCod)
+                    {
+                        alumnInCourse = true;
+                    }
+                }
+            }
+            return alumnInCourse;
+        }
+
+        public bool SelectedAlumnHasGrades(string aID)
+        {
+            bool hasGrades = false;
+            int personType = CheckTypeOfPerson(aID);
+            int personPosition = ReturnPersonPosition(aID);
+
+            // TYPE 0 IS ALUMN
+            if(personType == 0)
+            { 
+                if (((Alumn)personList[personPosition]).GradesList.Count() != 0)
+                {
+                    hasGrades = true;
+                }
+            }
+            return hasGrades;
+        }
+
+        public int AlumnsWithGradesCounter()
+        {
+            int alumnsWithGrades = 0;
+            for (int i = 0; i < personList.Count; i++)
+            {
+                string personID = ReturnsPersonIDFromPosition(i);
+                int personType = CheckTypeOfPerson(personID);
+
+                // TYPE 0 IS ALUMN
+                if (personType == 0)
+                {
+                    if (((Alumn)personList[i]).GradesList.Count() > 0)
+                    {
+                        alumnsWithGrades++;
+                    }
+                }
+            }
+            return alumnsWithGrades;
+        }
+
+        // END ALUMN RELATED METHODS //
+
+        public int GetPositionFromUniqueName(string pName)
+        {
+            int uniquePostion = 0;
+            for (int i = 0; i < personList.Count; i++)
+            {
+                if (pName == personList[i].Name)
+                {
+                    uniquePostion = i;
+                }
+            }
+            return uniquePostion;
         }
 
         public bool AlreadyUsedID(string pID)
@@ -74,16 +254,16 @@ namespace Exercise_5
 
         public string ReturnsPersonIDFromPosition(int pPosition)
         {
-            string alumnID = "";
+            string pID = "";
             bool found = false;
             for (int i = 0; i < personList.Count && !found; i++)
             {
                 if (pPosition == i)
                 {
-                    alumnID = personList[i].ID;
+                pID = personList[i].ID;
                 }
             }
-            return alumnID;
+            return pID;
         }
 
         public int CheckTypeOfPerson(string pID)
@@ -108,6 +288,27 @@ namespace Exercise_5
         public int CountsTotalPersons()
         {
             return personList.Count;
+        }
+
+        public int CountTotalAlumns()
+        {
+            if (CountsTotalPersons() > 0)
+            {
+                int totalAlumns = 0;
+                for(int i= 0; i < personList.Count; i++)
+                {
+                    int personType = CheckTypeOfPerson(person.ID);
+
+                    // TYPE 0 IS ALUMN
+                    if (personType == 0)
+                    {
+                        if (((Alumn).GradesList.Count() > 0)
+                        {
+                            totalAlumns++;
+                        }
+                    }
+                }
+            }
         }
     }
 }
