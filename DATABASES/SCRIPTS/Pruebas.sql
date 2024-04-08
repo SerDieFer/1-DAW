457e9498c2b333fdc910f1f51f1ad59ca21ed041
SerDieFer <serdiefer@alu.edu.gva.es>
2024-04-08 17:15:51 +0200

    BD

--- DATABASES/SCRIPTS/Pruebas.sql
+++ DATABASES/SCRIPTS/Pruebas.sql
@@ -1,48 +1,48 @@
 USE JARDINERIA
 
-DECLARE @varPrecio TINYINT
-SET @varPrecio = 75
+-- DECLARE @varPrecio TINYINT
+-- SET @varPrecio = 75
 
-SELECT *
-FROM PRODUCTOS
-WHERE precio_venta >= @varPrecio
+-- SELECT *
+-- FROM PRODUCTOS
+-- WHERE precio_venta >= @varPrecio
 
-DECLARE @codCategoria CHAR(2)
-SET @codCategoria = 'FR'
+-- DECLARE @codCategoria CHAR(2)
+-- SET @codCategoria = 'FR'
 
-DECLARE @nombre VARCHAR(100)
-SET @nombre = 'pera'
+-- DECLARE @nombre VARCHAR(100)
+-- SET @nombre = 'pera'
 
-SELECT *
-FROM CATEGORIA_PRODUCTOS ct, PRODUCTOS p
-WHERE ct.codCategoria = p.codCategoria
-AND LOWER(p.nombre) LIKE CONCAT(@nombre, '%')
-AND UPPER(ct.codCategoria) = @codCategoria
+-- SELECT *
+-- FROM CATEGORIA_PRODUCTOS ct, PRODUCTOS p
+-- WHERE ct.codCategoria = p.codCategoria
+-- AND LOWER(p.nombre) LIKE CONCAT(@nombre, '%')
+-- AND UPPER(ct.codCategoria) = @codCategoria
 
-DECLARE @precioMin DECIMAL(9,2)
-DECLARE @precioMax DECIMAL(9,2)
+-- DECLARE @precioMin DECIMAL(9,2)
+-- DECLARE @precioMax DECIMAL(9,2)
 
-SET @precioMin = 8.69
+-- SET @precioMin = 8.69
 
-SELECT TOP(1) @precioMin = precio_venta
-  FROM PRODUCTOS
- ORDER BY precio_venta ASC
+-- SELECT TOP(1) @precioMin = precio_venta
+--   FROM PRODUCTOS
+--  ORDER BY precio_venta ASC
 
- PRINT @precioMin
+--  PRINT @precioMin
 
- EXEC sp_columns EMPLEADOS
+--  EXEC sp_columns EMPLEADOS
 
-DECLARE @primerNombre  VARCHAR(50)
-DECLARE @apellido1     VARCHAR(50)
-DECLARE @apellido2     VARCHAR(50)
+-- DECLARE @primerNombre  VARCHAR(50)
+-- DECLARE @apellido1     VARCHAR(50)
+-- DECLARE @apellido2     VARCHAR(50)
 
-SELECT @primerNombre = nombre,
-       @apellido1 = apellido1,
-       @apellido2 = apellido2
-  FROM EMPLEADOS
- WHERE codEmpleado = 27
+-- SELECT @primerNombre = nombre,
+--        @apellido1 = apellido1,
+--        @apellido2 = apellido2
+--   FROM EMPLEADOS
+--  WHERE codEmpleado = 27
 
- PRINT CONCAT(@primerNombre,' ', @apellido1, ' ', @apellido2)
+--  PRINT CONCAT(@primerNombre,' ', @apellido1, ' ', @apellido2)
 
  
 -- DECLARE @i INT = 1
@@ -79,4 +79,90 @@ SELECT @primerNombre = nombre,
 -- END
 
 
+GO
+CREATE OR ALTER PROCEDURE holiwi
+AS
+BEGIN
+  PRINT 'Holis lokis'
+END 
 
+EXEC holiwi
+
+----------------------------------------------------------------
+
+GO
+CREATE OR ALTER PROCEDURE guessNumber 
+                            @number INT
+AS
+BEGIN
+  PRINT CONCAT('The number is: ', @number)
+  RETURN -1
+END
+GO
+
+DECLARE @number INT = FLOOR(RAND() * (100 - 1 + 1)) + 1
+DECLARE @return INT
+EXEC @return = guessNumber @number
+
+IF @return <> -1
+  RETURN
+PRINT 'Succesful procedure'
+
+----------------------------------------------------------------
+
+GO
+CREATE OR ALTER PROCEDURE guessNumber
+                            @number INT,
+                            @hemlo VARCHAR(5)
+AS
+BEGIN
+  PRINT CONCAT('The number is: ', @number)
+  PRINT @hemlo
+  RETURN -1
+END
+GO
+
+DECLARE @number INT = FLOOR(RAND() * (100 - 1 + 1)) + 1
+DECLARE @hemlo VARCHAR(5) = 'Hemlo'
+DECLARE @return INT
+
+EXEC @return = guessNumber
+                @number,
+                @hemlo
+
+IF @return <> -1
+  RETURN
+PRINT 'Succesful procedure'
+
+
+--------------------------------------------------------------------
+
+GO
+CREATE OR ALTER PROCEDURE printClientName 
+                            @cliCod INT,
+                            @cliName VARCHAR(125) OUTPUT
+AS
+BEGIN
+  SELECT @cliName = CONCAT('The client name is: ', nombre_cliente, CHAR(10), 'Whose representative is: ', nombre_contacto, ' ', apellido_contacto)
+  FROM CLIENTES
+  WHERE codCliente = @cliCod
+
+IF @cliName IS NULL
+  PRINT CONCAT('The the client Nº', @cliCod, ' does not exist')
+  RETURN -1
+END
+
+GO
+DECLARE @cliCod INT = 9
+DECLARE @return INT
+DECLARE @cliName VARCHAR(125)
+
+EXEC @return = printClientName 
+                @cliCod,
+                @cliName OUTPUT
+
+IF @return <> -1
+  RETURN
+
+PRINT @cliName
+PRINT CONCAT (CHAR(10), 'Succesful procedure')
