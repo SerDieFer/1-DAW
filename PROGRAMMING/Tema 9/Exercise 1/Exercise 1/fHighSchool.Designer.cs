84f1771738cf4446cb39c95c7359da82e68230cb
SerDieFer <serdiefer@alu.edu.gva.es>
2024-04-17 20:41:41 +0200

    PG

---
+++ PROGRAMMING/Tema 9/Exercise 1/Exercise 1/fHighSchool.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Exercise_1
+{
+    partial class fHighSchool
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurnames = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtbSurnames = new System.Windows.Forms.TextBox();
+            this.txtbPhone = new System.Windows.Forms.TextBox();
+            this.txtbEmail = new System.Windows.Forms.TextBox();
+            this.txtbName = new System.Windows.Forms.TextBox();
+            this.txtbID = new System.Windows.Forms.TextBox();
+            this.gbData = new System.Windows.Forms.GroupBox();
+            this.gpNavigate = new System.Windows.Forms.GroupBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.gbData.SuspendLayout();
+            this.gpNavigate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(23, 51);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(23, 16);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID:";
+            this.lblID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(23, 91);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "NAME:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSurnames
+            // 
+            this.lblSurnames.AutoSize = true;
+            this.lblSurnames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSurnames.Location = new System.Drawing.Point(23, 133);
+            this.lblSurnames.Name = "lblSurnames";
+            this.lblSurnames.Size = new System.Drawing.Size(87, 16);
+            this.lblSurnames.TabIndex = 2;
+            this.lblSurnames.Text = "SURNAMES:";
+            this.lblSurnames.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(23, 175);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(58, 16);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "PHONE:";
+            this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(23, 217);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(49, 16);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "EMAIL:";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtbSurnames
+            // 
+            this.txtbSurnames.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbSurnames.Location = new System.Drawing.Point(153, 130);
+            this.txtbSurnames.Name = "txtbSurnames";
+            this.txtbSurnames.Size = new System.Drawing.Size(178, 22);
+            this.txtbSurnames.TabIndex = 5;
+            // 
+            // txtbPhone
+            // 
+            this.txtbPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbPhone.Location = new System.Drawing.Point(153, 172);
+            this.txtbPhone.Name = "txtbPhone";
+            this.txtbPhone.Size = new System.Drawing.Size(178, 22);
+            this.txtbPhone.TabIndex = 6;
+            // 
+            // txtbEmail
+            // 
+            this.txtbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbEmail.Location = new System.Drawing.Point(153, 214);
+            this.txtbEmail.Name = "txtbEmail";
+            this.txtbEmail.Size = new System.Drawing.Size(178, 22);
+            this.txtbEmail.TabIndex = 7;
+            // 
+            // txtbName
+            // 
+            this.txtbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbName.Location = new System.Drawing.Point(153, 88);
+            this.txtbName.Name = "txtbName";
+            this.txtbName.Size = new System.Drawing.Size(178, 22);
+            this.txtbName.TabIndex = 8;
+            // 
+            // txtbID
+            // 
+            this.txtbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbID.Location = new System.Drawing.Point(153, 48);
+            this.txtbID.Name = "txtbID";
+            this.txtbID.Size = new System.Drawing.Size(178, 22);
+            this.txtbID.TabIndex = 9;
+            // 
+            // gbData
+            // 
+            this.gbData.Controls.Add(this.txtbID);
+            this.gbData.Controls.Add(this.txtbName);
+            this.gbData.Controls.Add(this.txtbEmail);
+            this.gbData.Controls.Add(this.txtbPhone);
+            this.gbData.Controls.Add(this.txtbSurnames);
+            this.gbData.Controls.Add(this.lblEmail);
+            this.gbData.Controls.Add(this.lblPhone);
+            this.gbData.Controls.Add(this.lblSurnames);
+            this.gbData.Controls.Add(this.lblName);
+            this.gbData.Controls.Add(this.lblID);
+            this.gbData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbData.Location = new System.Drawing.Point(12, 12);
+            this.gbData.Name = "gbData";
+            this.gbData.Size = new System.Drawing.Size(361, 262);
+            this.gbData.TabIndex = 10;
+            this.gbData.TabStop = false;
+            this.gbData.Text = "TEACHER DATA";
+            // 
+            // gpNavigate
+            // 
+            this.gpNavigate.Controls.Add(this.btnPrevious);
+            this.gpNavigate.Controls.Add(this.btnNext);
+            this.gpNavigate.Controls.Add(this.btnLast);
+            this.gpNavigate.Controls.Add(this.btnFirst);
+            this.gpNavigate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpNavigate.Location = new System.Drawing.Point(407, 12);
+            this.gpNavigate.Name = "gpNavigate";
+            this.gpNavigate.Size = new System.Drawing.Size(217, 262);
+            this.gpNavigate.TabIndex = 11;
+            this.gpNavigate.TabStop = false;
+            this.gpNavigate.Text = "NAVIGATE TO";
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFirst.Location = new System.Drawing.Point(21, 32);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(178, 41);
+            this.btnFirst.TabIndex = 0;
+            this.btnFirst.Text = "FIRST";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLast.Location = new System.Drawing.Point(21, 89);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(178, 41);
+            this.btnLast.TabIndex = 1;
+            this.btnLast.Text = "LAST";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(21, 145);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(178, 41);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.Location = new System.Drawing.Point(21, 203);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(178, 41);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "PREVIOUS";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // fHighSchool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 288);
+            this.Controls.Add(this.gpNavigate);
+            this.Controls.Add(this.gbData);
+            this.Name = "fHighSchool";
+            this.Text = "Highschool Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbData.ResumeLayout(false);
+            this.gbData.PerformLayout();
+            this.gpNavigate.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurnames;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtbSurnames;
+        private System.Windows.Forms.TextBox txtbPhone;
+        private System.Windows.Forms.TextBox txtbEmail;
+        private System.Windows.Forms.TextBox txtbName;
+        private System.Windows.Forms.TextBox txtbID;
+        private System.Windows.Forms.GroupBox gbData;
+        private System.Windows.Forms.GroupBox gpNavigate;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnFirst;
+    }
+}
+
---
+++ PROGRAMMING/Tema 9/Exercise 1/Exercise 1/fHighSchool.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Exercise_1
+{
+    public partial class fHighSchool : Form
+    {
+        public fHighSchool()
+        {
+            InitializeComponent();
+        }
+
+        DataSet dataSetTeachers;
+        SqlDataAdapter dataAdapaterTeachers;
+        private int pos;
+        private int maxRegistries;
+
+        private void showRegistry(int pos)
+        {
+            // Objeto que nos permite recoger un registro de la tabla.
+            DataRow dRegistry;
+
+            // Cogemos el registro de la posición pos en la tabla Profesores
+            dRegistry = dataSetTeachers.Tables["TEACHERS"].Rows[pos];
+
+            // Cogemos el valor de cada una de las columnas del registro
+            // y lo ponemos en el textBox correspondiente.
+
+            txtbID.Text = dRegistry[0].ToString();
+            txtbName.Text = dRegistry[1].ToString();
+            txtbSurnames.Text = dRegistry[2].ToString();
+            txtbPhone.Text = dRegistry[3].ToString();
+            txtbEmail.Text = dRegistry[4].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectLink = "Data Source =(LocalDB)\\MSSQLLocalDB;" +
+            "AttachDbFilename=C:\\Users\\serdiefer\\Desktop\\1-DAW\\PROGRAMMING\\Tema 9\\Exercise 1\\Exercise 1\\App_Data\\Highschool.mdf;" +
+            "Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(connectLink);
+
+            // OPENS CONNECTION
+            con.Open();
+
+            dataSetTeachers = new DataSet();
+            string stringSQL = "SELECT * FROM TEACHERS";
+
+            dataAdapaterTeachers = new SqlDataAdapter(stringSQL, con);
+
+            dataAdapaterTeachers.Fill(dataSetTeachers, "TEACHERS");
+
+            // SETS FIRST POSITION
+            pos = 0;
+            showRegistry(pos);
+
+            // GETS TOTAL REGISTRIES
+            maxRegistries = dataSetTeachers.Tables["TEACHERS"].Rows.Count;
+
+            // CLOSES CONNECTION
+            con.Close();
+
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            pos = 0;
+            showRegistry(pos);
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            pos = maxRegistries - 1;
+            showRegistry(pos);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (pos < maxRegistries)
+            {
+                pos++;
+                showRegistry(pos);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (pos >= 0)
+            {
+                pos--;
+                showRegistry(pos);
+            }
+        }
+    }
+}
