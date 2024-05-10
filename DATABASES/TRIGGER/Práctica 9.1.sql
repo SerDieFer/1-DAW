d36a96df752f1a7db2a75ee133a00c0db354da2c
SerDieFer <serdiefer@alu.edu.gva.es>
2024-05-10 17:27:06 +0200

    DB

--- DATABASES/TRIGGER/Práctica 9.1.sql
+++ DATABASES/TRIGGER/Práctica 9.1.sql
@@ -13,38 +13,40 @@ USE LIBRARY
 -- CREACIÓN DE LAS TABLAS DE LA LIBRERIA
 GO
 CREATE TABLE BOOKS(
-    ISBN CHAR(13) NOT NULL,
-    bookTitle VARCHAR(50) NOT NULL,
-    bookPrice DECIMAL(7,2) NOT NULL
+    ISBN        CHAR(13) NOT NULL,
+    bookTitle   VARCHAR(50) NOT NULL,
+    bookPrice   DECIMAL(7,2) NOT NULL
 
     CONSTRAINT PK_BOOKS PRIMARY KEY (ISBN)
 )
 GO
 CREATE TABLE MEMBERS(
-    ID CHAR(10) NOT NULL,
-    memberName VARCHAR(50) NOT NULL,
-    memberCity VARCHAR(50) NOT NULL
+    ID          CHAR(10) NOT NULL,
+    memberName  VARCHAR(50) NOT NULL,
+    memberCity  VARCHAR(50) NOT NULL
 
     CONSTRAINT PK_MEMBERS PRIMARY KEY (ID)
 )
 GO
 CREATE TABLE LOANS (
-    loanID INT NOT NULL,
-    loanData SMALLDATETIME NOT NULL,
-    loanReturn SMALLDATETIME,
-    bookISBN CHAR(13) NOT NULL,
-    memberID CHAR(10) NOT NULL
+    loanID      INT NOT NULL,
+    loanData    SMALLDATETIME NOT NULL,
+    loanReturn  SMALLDATETIME,
+    bookISBN    CHAR(13) NOT NULL,
+    memberID    CHAR(10) NOT NULL
 
     CONSTRAINT PK_LOANS PRIMARY KEY (loanID),
-    CONSTRAINT FK_BOOKSLOANS FOREIGN KEY (bookISBN) REFERENCES BOOKS(ISBN),
-    CONSTRAINT FK_MEMBERSLOANS FOREIGN KEY (memberID) REFERENCES MEMBERS(ID)
+    CONSTRAINT FK_BOOKSLOANS FOREIGN KEY (bookISBN) 
+    REFERENCES BOOKS(ISBN),
+    CONSTRAINT FK_MEMBERSLOANS FOREIGN KEY (memberID) 
+    REFERENCES MEMBERS(ID)
 )
 GO
 CREATE TABLE LOST_BOOKS (
-    ISBN CHAR(13) NOT NULL,
-    ID CHAR(10) NOT NULL,
-    memberName VARCHAR(50) NOT NULL,
-    lostDate SMALLDATETIME NOT NULL
+    ISBN         CHAR(13) NOT NULL,
+    ID           CHAR(10) NOT NULL,
+    memberName   VARCHAR(50) NOT NULL,
+    lostDate     SMALLDATETIME NOT NULL
 )   
 
 -- INSERTO REGISTROS PARA COMPROBAR EN LAS TABLAS
@@ -72,8 +74,6 @@ SELECT *
  WHERE 1 = 0
  ALTER TABLE MEMBERS_HISTORIC
    ADD changeDate SMALLDATETIME
-
-   --DROP TABLE MEMBERS_HISTORIC
 
 -- CREACION DEL TRIGGER CUANDO UN MIEMBRO ES BORRADO
 GO
@@ -132,6 +132,33 @@ BEGIN
     END CATCH
 END
 
+/* PRUEBA DEL TRIGGER
+
+BEGIN TRAN
+    BEGIN TRY
+        DELETE FROM MEMBERS
+        WHERE ID = '12345678A'
+           OR ID = '23456789B'
+        COMMIT
+    END TRY
+BEGIN CATCH
+    ROLLBACK
+END CATCH
+
+SELECT *
+FROM MEMBERS
+
+SELECT *
+FROM MEMBERS_HISTORIC
+
+SELECT *
+FROM BOOKS
+
+SELECT *
+FROM LOST_BOOKS
+
+*/
+
 ------------------
 --- EXERCISE 2 ---
 ------------------
@@ -147,38 +174,39 @@ USE TECHNICAL_SERVICE
 -- CREACIÓN DE LAS TABLAS DEL SERVICIO TÉCNICO
 GO
 CREATE TABLE TECHNICIANS(
-    ID CHAR(10) NOT NULL,
-    technicianName VARCHAR(50) NOT NULL,
-    technicianCity VARCHAR(50) NOT NULL,
+    ID               CHAR(10) NOT NULL,
+    technicianName   VARCHAR(50) NOT NULL,
+    technicianCity   VARCHAR(50) NOT NULL,
     technicianSalary DECIMAL(7,2) NOT NULL
 
     CONSTRAINT PK_TECHNICIANS PRIMARY KEY (ID)
 )
 GO
 CREATE TABLE REPAIRMENTS(
-    repairmentID INT NOT NULL,
-    repairmentOrderDate SMALLDATETIME NOT NULL,
-    repairConcept VARCHAR(50) NOT NULL,
-    repairCost DECIMAL(8,2) NOT NULL,
-    technicianID CHAR(10) NOT NULL
+    repairmentID         INT NOT NULL,
+    repairmentOrderDate  SMALLDATETIME NOT NULL,
+    repairConcept        VARCHAR(50) NOT NULL,
+    repairCost           DECIMAL(8,2) NOT NULL,
+    technicianID         CHAR(10) NOT NULL
 
     CONSTRAINT PK_REPAIRMENTS PRIMARY KEY (repairmentID),
-    CONSTRAINT FK_TECHNICIANS_REPAIRMENTS FOREIGN KEY (technicianID) REFERENCES TECHNICIANS(ID),
+    CONSTRAINT FK_TECHNICIANS_REPAIRMENTS FOREIGN KEY (technicianID) 
+    REFERENCES TECHNICIANS(ID),
 )
 
 -- INSERTO REGISTROS PARA COMPROBAR EN LAS TABLAS
 GO
 INSERT INTO TECHNICIANS (ID, technicianName, technicianCity, technicianSalary) 
-VALUES ('12345678A', 'Juan Pérez', 'Madrid', 2500.00),
-       ('23456789B', 'María López', 'Barcelona', 2700.00),
-       ('34567890C', 'David García', 'Valencia', 2600.00);
+VALUES ('12345678Z', 'Juan Pérez', 'Madrid', 2500.00),
+       ('23456789Q', 'María López', 'Barcelona', 2700.00),
+       ('34567890P', 'David García', 'Valencia', 2600.00)
 
 INSERT INTO REPAIRMENTS (repairmentID, repairmentOrderDate, repairConcept, repairCost, technicianID) 
-VALUES (1, '2024-05-01', 'Reparación de impresora', 1500.00, '12345678A'),
-       (2, '2024-05-02', 'Reparación de ordenador', 2550.00, '23456789B'),
-       (3, '2024-05-03', 'Reparación de móvil', 10000.00, '34567890C');
+VALUES (4, '2024-05-01', 'Reparación de impresora', 1500.00, '12345678Z'),
+       (5, '2024-05-02', 'Reparación de ordenador', 2550.00, '23456789Q'),
+       (6, '2024-05-03', 'Reparación de móvil', 10000.00, '34567890P')
 
--- CREACION DE BACKUP DE TECNICOS y REPARACIONES
+-- CREACION DE BACKUP DE TECNICOS Y REPARACIONES
 GO
 SELECT *
   INTO TECHNICIANS_BACKUP
@@ -206,7 +234,7 @@ BEGIN
         
         IF @hasTransaction = 0
         BEGIN TRAN
-    
+
             -- ACTUALIZACIÓN DE LOS DATOS BORRADOS A LA TABLA DE RESERVA DE TECNICOS (BACKUP)
             INSERT INTO TECHNICIANS_BACKUP
             SELECT DELETED.*,
@@ -248,12 +276,13 @@ BEGIN
     END CATCH
 END
 
-/*
+/* PRUEBA DEL TRIGGER
+
 BEGIN TRAN
     BEGIN TRY
         DELETE FROM TECHNICIANS
-        WHERE ID = '12345678A'
-           OR ID = '23456789B'
+        WHERE ID = '12345678Z'
+           OR ID = '23456789Q'
         COMMIT
     END TRY
 BEGIN CATCH
@@ -271,12 +300,259 @@ FROM TECHNICIANS_BACKUP
 
 SELECT *
 FROM TECHNICIANS
+
 */
 
+------------------
+--- EXERCISE 3 ---
+------------------
+
+----------------------------------------------------------------------
+
+-- CREACIÓN DE LA BASE DE DATOS DEL ALMACEN Y POSTERIOR USO DE ESTE
+GO
+CREATE DATABASE WAREHOUSE_MANAGEMENT
+GO
+USE WAREHOUSE_MANAGEMENT
+
+-- CREACIÓN DE LAS TABLAS DE LA GESTION DE ALMACÉN
+GO
+CREATE TABLE WAREHOUSES(
+    warehouseID             INT NOT NULL,
+    warehouseDescription    VARCHAR(100) NOT NULL,
+    warehouseAdress         VARCHAR(75) NOT NULL,
+    warehouseCity           VARCHAR(50) NOT NULL
+
+    CONSTRAINT PK_WAREHOUSE PRIMARY KEY (warehouseID)
+)
+GO
+CREATE TABLE SUPPLIERS(
+    supplierID      INT NOT NULL,
+    supplierName    VARCHAR(75) NOT NULL,
+    supplierAdress  VARCHAR(75) NOT NULL,
+    supplierCity    VARCHAR(50) NOT NULL,
+    supplierDebt    DECIMAL(8,2) NOT NULL,
+    supplierType    VARCHAR(20) NOT NULL
+
+    CONSTRAINT PK_SUPPLIERS PRIMARY KEY (supplierID)
+)
+GO
+CREATE TABLE GOODS(
+    goodID       INT NOT NULL,
+    goodName     VARCHAR(50) NOT NULL,
+    goodStock    INT NOT NULL,
+    goodPVP      DECIMAL(8,2) NOT NULL,
+    goodPrice    DECIMAL(8,2) NOT NULL,
+    warehouseID  INT NOT NULL,
+    supplierID   INT NULL
+
+    CONSTRAINT PK_GOODS PRIMARY KEY (goodID),
+    CONSTRAINT FK_WAREHOUSES_GOODS FOREIGN KEY (warehouseID) 
+    REFERENCES WAREHOUSES(warehouseID),
+    CONSTRAINT FK_SUPPLIERS_GOODS FOREIGN KEY (supplierID) 
+    REFERENCES SUPPLIERS(supplierID),
+)
+
+-- INSERTO REGISTROS PARA COMPROBAR EN LAS TABLAS
+GO
+INSERT INTO WAREHOUSES (warehouseID, warehouseDescription, warehouseAdress, warehouseCity)
+VALUES (1, 'Central Warehouse', '456 Oak St', 'City I'),
+       (2, 'West Warehouse', '789 Pine St', 'City J'),
+       (3, 'East Warehouse', '123 Cedar St', 'City K'),
+       (4, 'North Warehouse', '555 Elm St', 'City L')
+GO
+INSERT INTO SUPPLIERS (supplierID, supplierName, supplierAdress, supplierCity, supplierDebt, supplierType)
+VALUES (1, 'Supplier 1', '777 Maple St', 'City M', 1200.00, 'Type B'),
+       (2, 'Supplier 2', '888 Pine St', 'City N', 900.00, 'Type C'),
+       (3, 'Supplier 3', '999 Cedar St', 'City O', 1500.00, 'Type A'),
+       (4, 'Supplier 4', '111 Oak St', 'City P', 600.00, 'Type B')
+GO
+INSERT INTO GOODS (goodID, goodName, goodStock, goodPVP, goodPrice, warehouseID, supplierID)
+VALUES (1, 'Good 1', 80, 25.00, 20.00, 1, 1),
+       (2, 'Good 2', 0, 35.00, 30.00, 2, 2),
+       (3, 'Good 3', 90, 45.00, 40.00, 3, 2),
+       (4, 'Good 4', 0, 55.00, 50.00, 4, 4)
+
+-- CREACION DE BACKUP DE ARTICULOS Y PROVEEDORES
+GO
+SELECT *
+  INTO GOODS_NOTEXISTANCE
+  FROM GOODS
+ WHERE 1 = 0
+ ALTER TABLE GOODS_NOTEXISTANCE
+   ADD goodNoStock SMALLDATETIME,
+       supplierName VARCHAR(75)
+GO
+SELECT *
+  INTO SUPPLIERS_BACKUP
+  FROM SUPPLIERS
+ WHERE 1 = 0
+ ALTER TABLE SUPPLIERS_BACKUP
+   ADD supplierLeaveDate SMALLDATETIME
+
+-- CREACION DEL TRIGGER CUANDO SE DA DE BAJA UN PROVEEDOR
+GO
+CREATE OR ALTER TRIGGER TX_SUPPLIER_LEAVE ON SUPPLIERS INSTEAD OF DELETE
+AS
+BEGIN
+    BEGIN TRY
+        DECLARE @hasTransaction BIT = 1
+
+        -- SI NO EXISTEN TRANSACCIONES ABIERTAS SE INICIA UNA TRANSACCION
+        IF @@TRANCOUNT = 0 
+            SET @hasTransaction = 0
         
+        IF @hasTransaction = 0
+        BEGIN TRAN
 
-    
+            -- ACTUALIZACIÓN DE LOS DATOS BORRADOS A LA TABLA DE ARTICULOS NO EXISTENTES
+            INSERT INTO GOODS_NOTEXISTANCE
+            SELECT GOODS.*,
+                   GETDATE(),
+                   DELETED.supplierName
+              FROM GOODS,
+                   DELETED
+             WHERE DELETED.supplierID = GOODS.supplierID
+               AND GOODS.goodStock = 0
 
+            -- COPIA DEL BACKUP DE PROVEEDORES
+            INSERT INTO SUPPLIERS_BACKUP
+            SELECT *,
+                   GETDATE()
+              FROM SUPPLIERS
+             WHERE supplierID IN (SELECT supplierID
+                                    FROM DELETED)
+ 
+            -- BORRADO EN CASCADA DE LOS DATOS DEL PRODUCTO Y EL PROVEEDOR A BORRAR
+            DELETE FROM GOODS
+             WHERE supplierID IN (SELECT supplierID
+                                    FROM DELETED)
+               AND goodStock = 0
 
+            UPDATE GOODS
+               SET supplierID = NULL
+             WHERE supplierID IN (SELECT supplierID
+                                    FROM DELETED)
+               AND goodStock > 0
 
+            DELETE FROM SUPPLIERS
+             WHERE supplierID IN (SELECT supplierID
+                                    FROM DELETED)
 
+        -- SI SOLO EXISTE LA TRANSACCION PREVIA SE HACE COMMIT
+        IF @hasTransaction = 0
+        COMMIT
+    END TRY
+    BEGIN CATCH
+        PRINT CONCAT ('CODERROR: ', ERROR_NUMBER(), CHAR(10),
+                      'DESCRIPTION: ', ERROR_MESSAGE(), CHAR(10),
+                      'LINE: ', ERROR_LINE(), CHAR(10),
+                      'PROCEDURE: ', ERROR_PROCEDURE())
+        -- SI SOLO EXISTE LA TRANSACCION PREVIA SE HACE COMMIT
+        IF @hasTransaction = 0
+        ROLLBACK
+    END CATCH
+END
+
+/* PRUEBA DEL TRIGGER
+
+BEGIN TRAN
+    BEGIN TRY
+        DELETE FROM SUPPLIERS
+        WHERE supplierID = '1'
+           OR supplierID = '2'
+        COMMIT
+    END TRY
+BEGIN CATCH
+    ROLLBACK
+END CATCH
+
+SELECT *
+FROM SUPPLIERS
+
+SELECT *
+FROM SUPPLIERS_BACKUP
+
+SELECT *
+FROM GOODS
+
+SELECT *
+FROM GOODS_NOTEXISTANCE
+
+*/
+
+------------------
+--- EXERCISE 4 ---
+------------------
+
+----------------------------------------------------------------------------------------------------
+
+GO
+USE JARDINERIA
+
+-- CREACION DE BACKUP DE CLIENTES
+GO
+SELECT *
+  INTO CLIENTES_BACKUP
+  FROM CLIENTES
+ WHERE 1 = 0
+ ALTER TABLE CLIENTES_BACKUP
+   ADD clienteUpdateDate SMALLDATETIME,
+       CONSTRAINT PK_CLIENTES_BACKUP PRIMARY KEY(clienteUpdateDate)
+
+-- CREACION DEL TRIGGER CUANDO SE DA ACTUALIZA UN PROVEEDOR
+GO
+CREATE OR ALTER TRIGGER TX_CLIENT_UPDATE ON CLIENTES AFTER UPDATE
+AS
+BEGIN
+    BEGIN TRY
+        DECLARE @hasTransaction BIT = 1
+
+        -- SI NO EXISTEN TRANSACCIONES ABIERTAS SE INICIA UNA TRANSACCION
+        IF @@TRANCOUNT = 0 
+            SET @hasTransaction = 0
+        
+        IF @hasTransaction = 0
+        BEGIN TRAN
+
+            -- ACTUALIZACIÓN DE LOS DATOS BORRADOS A LA TABLA DE RESERVA DE TECNICOS (BACKUP)
+            INSERT INTO CLIENTES_BACKUP
+            SELECT INSERTED.*,
+                   GETDATE()
+              FROM INSERTED
+              
+        -- SI SOLO EXISTE LA TRANSACCION PREVIA SE HACE COMMIT
+        IF @hasTransaction = 0
+        COMMIT
+    END TRY
+    BEGIN CATCH
+        PRINT CONCAT ('CODERROR: ', ERROR_NUMBER(), CHAR(10),
+                      'DESCRIPTION: ', ERROR_MESSAGE(), CHAR(10),
+                      'LINE: ', ERROR_LINE(), CHAR(10),
+                      'PROCEDURE: ', ERROR_PROCEDURE())
+        -- SI SOLO EXISTE LA TRANSACCION PREVIA SE HACE COMMIT
+        IF @hasTransaction = 0
+        ROLLBACK
+    END CATCH
+END
+
+/* PRUEBA DEL TRIGGER
+
+BEGIN TRAN
+    BEGIN TRY
+        UPDATE CLIENTES
+        SET nombre_cliente = 'Abel Pls'
+        WHERE codCliente = 1
+        COMMIT
+    END TRY
+BEGIN CATCH
+    ROLLBACK
+END CATCH
+
+SELECT *
+FROM CLIENTES
+
+SELECT *
+FROM CLIENTES_BACKUP
+
+*/
